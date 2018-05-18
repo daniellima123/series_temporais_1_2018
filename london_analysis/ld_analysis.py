c6f34591be83d476970c42d6fd9d2f7959b8bd4c
Daniel <daniel.limaviegas454@gmail.com>
2018-05-17 23:15:21 -0300

    continuar amanhã

--- london_analysis/ld_analysis.py
+++ london_analysis/ld_analysis.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from statsmodels.graphics.tsaplots import plot_acf
+import scipy as sp
+import numpy as np
 
-def criador_de_serie(df):
-    valor = []
-    for i in range(0, df.shape[0]):
-        aux = df.iloc[i, 2:].max()
-        valor.append(aux)
-    serie = pd.DataFrame({'Station ID':df['Station ID'],
-                          'Pollutant': df['Pollutant'],
-                          'Valor': valor}, index=df.index)
-
-    return serie
 if __name__ == '__main__':
 
 
-    london_df = pd.read_csv('Matter2013-London.csv', index_col='Date').iloc[:,:-1]
+    london_df = pd.read_csv('Matter2013-London.csv', index_col='Date',
+                            parse_dates=True).iloc[:,:-1]
     london_df = london_df.applymap(lambda x: None if(x in (-999, 9999)) else x)
 
-    serie = criador_de_serie(london_df)
+    london_df['Maximo'] = london_df.filter(regex="^H").max(axis=1)
 
-    _ = serie.Valor.plot(grid=True)
+    # _ = serie.Valor.plot(grid=True)
+    # plt.show()
+    autocorrelation = london_df['Maximo'].autocorr()
+    vari = 1/len(london_df)
+    z = sp.stats.norm.ppf(0.95)
+    interval = [-z*np.sqrt(vari), z*np.sqrt(vari)]
+
+    plot_acf(london_df.Maximo, alpha=1, marker='')
+    plt.hlines(y = interval, xmin=0, xmax=30)
     plt.show()
+    print("A auto correlação dos dados diários é: %4.2f" %(autocorrelation))
